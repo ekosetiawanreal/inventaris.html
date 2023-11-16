1e350e6fb6b73b5deea1d26b62c4aa56b055bd17
eko setiawan <151000898+ekosetiawanreal@users.noreply.github.com>
2023-11-16 13:34:36 +0700

    Add files via upload

---
+++ inventaris/page/edit_barang.php
@@ -0,0 +1,46 @@
+<div class="row">
+    <div class="col-lg-4">
+    <div class="panel panel-primary">
+        <div class="panel-heading"> edit Inventaris</div>
+            <div class="panel-body">
+                        <form action="">
+                            <div class="form-group">
+                            <label for=""> Kode inventaris</label>
+                            <input type="text" class="form-control" placeholder="Masukkan Kode Barang">
+                            </div>
+                            <div class="form-group">
+                            <label for=""> Nama inventaris</label>
+                            <input type="text" class="form-control" placeholder="Masukkan Nama Barang">
+                            </div>
+                            <div class="form-group">
+                            <label for=""> Kondisi</label>
+                        <select name="" id="" class="form-control">
+                            <option value="" name="" class="form-control">- Pilih -</option>
+                            <option value="" name="" class="form-control">Baik</option>
+                            <option value="" name="" class="form-control">Rusak</option>
+                        </select>
+                            </div>
+                            <div class="form-group">
+                            <label for="">Jumlah</label>
+                            <input type="number" class="form-control" placeholder="Masukkan jumlah Barang">
+                            </div>
+                            <div class="form-group">
+                            <label for=""> Nama ruang</label>
+                            <input type="text" class="form-control" placeholder="Masukkan Nama Ruang">
+                            </div>
+                            <div class="form-group">
+                            <label for="">Keterangan</label>
+                        <textarea name="" id="" cols="30" rows="5" class="form-control"></textarea>
+                            </div>
+                            <div class="form-group">
+                        <button class="btn btn-md btn-primary ">Simpan</button>
+                        <a href="?p=list_barang" class="btn btn-md btn-default">Kembali</a>
+                            </div>
+                        </form>
+                </div>
+            </div>
+    
+    </div>
+    
+
+</div>
---
+++ inventaris/page/home.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+        <h1>Selamat Datang, <?= $user ?>! </h1>
+        <p>SELAMAT DATANG DI APLIKASI INVENTARIS KANTOR .</p>
+        <p>Aplikasi ini merupakan aplikasi untuk uji level kelas XII RPL .</p>
+        
+      </div>
